15db9a4b0b2b7f0b9e05b240f120bdca31f8a9d0
Fernando <me@fdorado.com>
2023-02-14 12:47:29 -0500

    Created preliminary Navbar component.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+export default function Navbar () {
+    return (
+        <nav className="navbar-container">
+            <h1>Fernando Dorado</h1>
+            <div className="nav-cta-container">
+                <ul>
+                    <li>About</li>
+                    <li>Experience</li>
+                    <li>Work</li>
+                    <li>Contact</li>
+                </ul>
+                <button>Resume</button>
+            </div>
+        </nav>
+    )
+}
